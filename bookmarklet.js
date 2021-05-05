7b785f00f982df6aac963e07d7cc6c9f2d95dab8
Colin Devroe <colin@cdevroe.com>
2021-05-05 10:21:18 -0400

    Initial commit

---
+++ bookmarklet.js
@@ -0,0 +1,13 @@
+javascript: (function (){
+    "use strict";
+    var w = window,
+        d = document,
+        s = w.getSelection().toString(),
+        u = '';
+        if ( s.length > 0 ) { // There is a selection
+            u = 'https://micro.blog/post?text=>%20' + encodeURIComponent(s) + encodeURIComponent('&#13;&#13;') + 'Source:%20[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
+        } else {
+            u = 'https://micro.blog/post?text=[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
+        }
+    w.open(u, 'Post to Micro.blog', 'location=0,links=0,scrollbars=0,toolbar=0,width=594,height=615');
+})();
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Micro.blog Posting Bookmarklet
+
+By: [Colin Devroe](http://cdevroe.com/projects/mb-bookmarklet)
+
+A simple bookmarklet to post links to Micro.blog based on the current page in your desktop browser. Supports text selections.
+
+If you'd like to support the creation and maintanence of this bookmarklet please consider [donating](http://cdevroe.com/donate).
+
+## How to install
+
+Visit [the project page](http://cdevroe.com/projects/mb-bookmarklet) for details on how to install this bookmarklet.
+
+## How to contribute
+
+Submit any issues you have or submit a Pull Request with updates!
+
+## Version History
+
+2021.1.0 - Released: May 5, 2021
+
+- Initial release
