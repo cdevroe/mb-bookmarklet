4f4bfd908c56ee88a75f88d91a6f15c2274f9fed
Colin Devroe <colin@cdevroe.com>
2021-05-06 15:15:29 -0400

    Added bookmarklet=true for micro.blog

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,9 +5,9 @@ javascript: (function (){
         s = w.getSelection().toString(),
         u = '';
         if ( s.length > 0 ) { // There is a selection
-            u = 'https://micro.blog/post?text=>%20' + encodeURIComponent(s) + encodeURIComponent('&#13;&#13;') + 'Source:%20[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
+            u = 'https://micro.blog/post?bookmarklet=true&text=>%20' + encodeURIComponent(s) + encodeURIComponent('&#13;&#13;') + 'Source:%20[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
         } else {
-            u = 'https://micro.blog/post?text=[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
+            u = 'https://micro.blog/post?bookmarklet=true&text=[' + encodeURIComponent(d.title) + '](' + encodeURIComponent(w.location.href) + ')';
         }
     w.open(u, 'Post to Micro.blog', 'location=0,links=0,scrollbars=0,toolbar=0,width=594,height=615');
 })();
--- readme.md
+++ readme.md
@@ -16,6 +16,10 @@ Submit any issues you have or submit a Pull Request with updates!
 
 ## Version History
 
+2021.2.0 - Released: May 6, 2021
+
+- Added bookmarklet variable for Micro.blog to know to close the pop-up after Posting - Requested by [@maique](http://micro.blog/maique)
+
 2021.1.0 - Released: May 5, 2021
 
 - Initial release
